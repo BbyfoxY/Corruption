7817550adb2d3ec672ef73a71aecd47da61e3be1
BbyfoxY <77471733+BbyfoxY@users.noreply.github.com>
2022-03-23 03:09:43 +0000

    Time to test!

--- Shade/index.js
+++ Shade/index.js
@@ -1,11 +1,1 @@
-/*
-new Nightmare({
-	"title": "Shade",
-	"type": "url",
-	"url": "./Shade/",
-	"width": 800,
-	"height": 600,
-	"theme": "dark"
-});
-*/
-var Shade = window.open('./Shade/', 'window', 'location = no, titlebar = no, toolbar = no, menubar = no, resizable = yes');
+var Shade = window.open('./s/', 'window', 'location = no, titlebar = no, toolbar = no, menubar = no, resizable = yes');
--- Shade/s/index.js
+++ Shade/s/index.js
@@ -3,6 +3,9 @@ function main() {
 	var t = document.createElement('h1');
 	var ta = document.createElement('a');
 	var tat = document.createTextNode('Shade');
-	ta.href = '/index.html'
+	ta.href = '../';
+	ta.appendChild(tat);
+	t.appendChild(ta);
+	title.appendChild(t);
 }
-//main();
+main();
